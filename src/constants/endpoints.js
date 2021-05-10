f3e3b7a15f8474cb09ea6be2d60e743e8dadd5f6
jacoblimzm <jacob_lim@live.com>
2021-05-10 12:17:53 +0800

    add constants for api paths

---
+++ src/constants/endpoints.js
@@ -0,0 +1,27 @@
+
+// for "accounts" route
+const logInPath = "accounts/user/login/"
+const logOutPath = "account/user/logout/"
+const registerPath = "accounts/user/register/"
+const apiTokenPath = "accounts/api/token/" 
+const apiRefreshTokenPath = "accounts/api/token/refresh/"
+
+// for "api-blog" route
+const allPostsPath = "api-blog/"
+const allCategoriesPath = "api-blog/categories/"
+const newPostPath = "api-blog/new/"
+
+// path("categories/<category_name>", views.CategoryDetailView.as_view()),
+// path("<slug>/", views.BlogEntryDetailView.as_view()),
+// path("<slug>/edit", views.BlogEntryEditView.as_view()),
+
+export { 
+    logInPath, 
+    logOutPath, 
+    registerPath, 
+    apiTokenPath, 
+    apiRefreshTokenPath, 
+    allPostsPath,
+    allCategoriesPath,
+    newPostPath
+}
