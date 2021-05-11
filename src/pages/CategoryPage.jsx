ccd39a8b3a9f8f447ce6bf24214d3757bf2a4d85
jacoblimzm <jacob_lim@live.com>
2021-05-11 11:25:54 +0800

    import Header component

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,6 +1,12 @@
+import {useParams} from "react-router-dom"
+import Header from "../components/Header";
+import { capitalise } from "../constants/functions";
 
 const CategoryPage = () => {
+
+    const {categoryName} = useParams()
     return ( 
+        <Header header={capitalise(categoryName)} />
 
      );
 }
