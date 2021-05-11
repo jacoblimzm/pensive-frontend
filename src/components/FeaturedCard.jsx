8865a546d999ea00e113b7fc93416d6f5cb151f6
jacoblimzm <jacob_lim@live.com>
2021-05-11 11:18:26 +0800

    remove inline featured header

--- src/components/FeaturedCard.jsx
+++ src/components/FeaturedCard.jsx
@@ -4,7 +4,7 @@ const FeaturedCard = ( {title, excerpt, slug} ) => {
   return (
     <div className="p-4 p-md-5 mb-4 text-white rounded bg-secondary" id="featured-div">
       <div className="col px-0">
-        <h2 style={{ textAlign: "center" }}>featured</h2>
+        {/* <h2 style={{ textAlign: "center" }}>featured</h2> */}
         <h1 className="display-4 fst-italic">{title}</h1>
         <p className="lead my-3">{excerpt}</p>
         <p className="lead mb-0">
