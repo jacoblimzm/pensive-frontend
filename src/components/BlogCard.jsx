32f6f7bc10c8d4e8814f04740069f25824be25d9
jacoblimzm <jacob_lim@live.com>
2021-05-11 14:24:07 +0800

    change category font colour

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -8,7 +8,7 @@ const BlogCard = ( {title, category, month, day, excerpt, slug, image} ) => {
         <div class="col-md-6">
           <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
             <div class="col p-4 d-flex flex-column position-static">
-              <strong class="d-inline-block mb-2 text-primary">
+              <strong class="d-inline-block mb-2 text-success">
                 {capitalise(category.category_name)}
               </strong>
               <h3 class="mb-0">{title}</h3>
