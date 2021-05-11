f97f99f72b97be09f7d3f6200a84b3f1052263dd
jacoblimzm <jacob_lim@live.com>
2021-05-11 12:49:48 +0800

    set up BlogDetail structure

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -1,10 +1,28 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const BlogDetail = () => {
   const { slugName } = useParams();
   return (
     <div className="container">
-      <h1 className="fst-italic">{slugName}</h1>
+      <img className="img-fluid" />
+      <h1 className="display-3 fst-italic mb-2">{slugName}</h1>
+      <h3 className="mb-4">
+        {" "}
+        Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt{" "}
+      </h3>
+      <h4 className="text-secondary"> Category: Something</h4>
+      <h6>Month / Day</h6>
+      
+      <hr />
+      <p>
+        Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt
+        Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt
+        Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt
+        Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt
+        Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt
+      </p>
+      <hr />
+      <Link to="/">Back to Posts</Link>
     </div>
   );
 };
