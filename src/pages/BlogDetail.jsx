da92d656e24b14076b1ad28e8132f785b35ace06
jacoblimzm <jacob_lim@live.com>
2021-05-11 14:51:15 +0800

    fix formatting

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -5,35 +5,38 @@ import { allPostsPath } from "../constants/endpoints";
 import { capitalise } from "../constants/functions";
 
 const BlogDetail = () => {
-    const { slugName } = useParams();
+  const { slugName } = useParams();
 
-    const [blogPost, setBlogPost] = useState({})
+  const [blogPost, setBlogPost] = useState({});
 
-    const getBlogPost = async (slug) => {
-        try {
-            const response = await axios.get(`${allPostsPath}${slug}/`);
-            console.log(response.data);
-            setBlogPost(response.data)
-        } catch(err) {
-            console.log(err.response);
-        }
+  const getBlogPost = async (slug) => {
+    try {
+      const response = await axios.get(`${allPostsPath}${slug}/`);
+      console.log(response.data);
+      setBlogPost(response.data);
+    } catch (err) {
+      console.log(err.response);
     }
+  };
 
-    useEffect( () => {
-        getBlogPost(slugName);
-
-    }, [])
+  useEffect(() => {
+    getBlogPost(slugName);
+  }, []);
   return (
     <div className="container">
-      <img className="img-fluid" />
-      <h1 className="display-3 fst-italic mb-2">{blogPost.title}</h1>
-      <h3 className="mb-4">
-        {blogPost.excerpt}
-      </h3>
-      <h4 className="text-success">{blogPost.category && capitalise(blogPost.category.category_name)}</h4>
-      <h6>{blogPost.month} {blogPost.day}</h6>
+      <img src={blogPost.image} className="img-fluid shadow-sm" alt={blogPost.title}/>
+      <h1 className="display-3 fst-italic my-2">{blogPost.title}</h1>
+      <h3 className="mb-4">{blogPost.excerpt}</h3>
+      <h4 className="text-success">
+        {blogPost.category && capitalise(blogPost.category.category_name)}
+      </h4>
+      <h6>
+        {blogPost.month} {blogPost.day}
+      </h6>
       <hr />
-      {blogPost.content}
+      <div className="row">
+        <div className="col-sm-12 col-md-8"><p className="blog-content">{blogPost.content}</p></div>
+      </div>
       <hr />
       <Link to="/">Back to Posts</Link>
     </div>
