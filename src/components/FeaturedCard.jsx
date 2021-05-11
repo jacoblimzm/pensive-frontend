41c95923f46fcd5a198395e09b8abe0346e434db
jacoblimzm <jacob_lim@live.com>
2021-05-11 10:49:01 +0800

    create FeaturedCard component

---
+++ src/components/FeaturedCard.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+
+const FeaturedCard = ( {title, excerpt, slug} ) => {
+  return (
+    <div className="p-4 p-md-5 mb-4 text-white rounded bg-secondary" id="featured-div">
+      <div className="col px-0">
+        <h2 style={{ textAlign: "center" }}>featured</h2>
+        <h1 className="display-4 fst-italic">{title}</h1>
+        <p className="lead my-3">{excerpt}</p>
+        <p className="lead mb-0">
+          <Link className="text-white fw-bold" to={`blog/${slug}`}>
+            Continue reading....
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedCard;
