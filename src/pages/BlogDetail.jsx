32fd2bce07dae1e5f1ba8e02a9dbf390cf3843d7
jacoblimzm <jacob_lim@live.com>
2021-05-11 14:24:34 +0800

    complete BlogDetail

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -1,26 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { allPostsPath } from "../constants/endpoints";
+import { capitalise } from "../constants/functions";
 
 const BlogDetail = () => {
-  const { slugName } = useParams();
+    const { slugName } = useParams();
+
+    const [blogPost, setBlogPost] = useState({})
+
+    const getBlogPost = async (slug) => {
+        try {
+            const response = await axios.get(`${allPostsPath}${slug}/`);
+            console.log(response.data);
+            setBlogPost(response.data)
+        } catch(err) {
+            console.log(err.response);
+        }
+    }
+
+    useEffect( () => {
+        getBlogPost(slugName);
+
+    }, [])
   return (
     <div className="container">
       <img className="img-fluid" />
-      <h1 className="display-3 fst-italic mb-2">{slugName}</h1>
+      <h1 className="display-3 fst-italic mb-2">{blogPost.title}</h1>
       <h3 className="mb-4">
-        {" "}
-        Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt{" "}
+        {blogPost.excerpt}
       </h3>
-      <h4 className="text-secondary"> Category: Something</h4>
-      <h6>Month / Day</h6>
-      
+      <h4 className="text-success">{blogPost.category && capitalise(blogPost.category.category_name)}</h4>
+      <h6>{blogPost.month} {blogPost.day}</h6>
       <hr />
-      <p>
-        Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt
-        Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt
-        Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt
-        Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt
-        Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt Excerpt
-      </p>
+      {blogPost.content}
       <hr />
       <Link to="/">Back to Posts</Link>
     </div>
