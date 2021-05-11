2d4dc9f6d985cf3aef03891d87703c76ca24707c
jacoblimzm <jacob_lim@live.com>
2021-05-11 16:45:58 +0800

    add form validation for existing user

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,34 +1,57 @@
+import axios from "axios";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Form, useForm } from "../components/useForm";
-
+import { registerPath } from "../constants/endpoints";
 
 const RegisterPage = () => {
-    const [registerSuccess, setRegisterSuccess] = useState(null)
-    const initialUserValues = {
-        username: "",
-        password: "",
-        email: "",
-    }
+  const [registerSuccess, setRegisterSuccess] = useState({});
+  const initialUserValues = {
+    username: "",
+    password: "",
+    email: "",
+  };
 
-    const { formValues, handleInputChange } = useForm(initialUserValues);
+  const { formValues, handleInputChange } = useForm(initialUserValues);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(e.target)
+  const register = async (username, password, email, is_staff) => {
+      try {
+          const response = await axios.post(registerPath, {
+              username: username,
+              password: password,
+              email: email,
+              is_staff: is_staff,
+          })
+          console.log(response.data)
+          setRegisterSuccess(response.data)
+      } catch(err) {
+          console.log(err.response.data)
+        //   setRegisterSuccess(err.response.data.success);
+      }
+  }
 
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+    const email = e.target.email.value;
+    const is_staff = e.target.staffstatus.checked;
+    register(username, password, email, is_staff)
+
+  };
 
   return (
     <main className="container form-container">
       <Form handleSubmit={handleSubmit}>
         <h3 className="mb-3">REGISTER FOR AN ACCOUNT</h3>
-        <p>Please do not leave any fields blank</p>
+        <p>Please do NOT leave any fields blank.</p>
         <div className="mb-3">
           <label htmlFor="register-email" className="form-label">
             Email
           </label>
           <input
+            required
             name="email"
             value={formValues.email}
             onChange={handleInputChange}
@@ -43,6 +66,7 @@ const RegisterPage = () => {
             Username
           </label>
           <input
+            required
             name="username"
             value={formValues.username}
             onChange={handleInputChange}
@@ -51,15 +75,13 @@ const RegisterPage = () => {
             id="register-username"
             aria-describedby="usernameField"
           />
-          {/* <div id="emailHelp" className="form-text">
-              We'll never share your email with anyone else.
-            </div> */}
         </div>
         <div className="mb-3">
           <label htmlFor="register-password" className="form-label">
             Password
           </label>
           <input
+            required
             name="password"
             value={formValues.password}
             onChange={handleInputChange}
@@ -69,9 +91,21 @@ const RegisterPage = () => {
             aria-describedby="passwordField"
           />
         </div>
-        {/* {registerSuccess === false ? (
-          <p style={{ color: "red" }}>Please Try Again</p>
-        ) : null} */}
+        <div className="form-check mb-3">
+          <input
+            name="staffstatus"
+            className="form-check-input"
+            type="checkbox"
+            value="staff-status"
+            id="staffCheckDefault"
+          />
+          <label className="form-check-label" htmlFor="staffCheckDefault">
+            Staff
+          </label>
+        </div>
+        {registerSuccess.success === false ? (
+          <p style={{ color: "red" }}>{registerSuccess.message}</p>
+        ) : null}
         <button className="w-100 btn btn-lg btn-dark mb-2" type="submit">
           Register
         </button>
