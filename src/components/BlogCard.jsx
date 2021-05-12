a2ef7776adb676db0206afab03c26b2888eb140e
jacoblimzm <jacob_lim@live.com>
2021-05-12 17:33:12 +0800

    fix class name warning

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -5,27 +5,27 @@ import { capitalise } from "../constants/functions";
 
 const BlogCard = ( {title, category, month, day, excerpt, slug, image} ) => {
     return ( 
-        <div class="col-md-6">
-          <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div class="col p-4 d-flex flex-column position-static">
-              <strong class="d-inline-block mb-2 text-success">
+        <div className="col-md-6">
+          <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+            <div className="col p-4 d-flex flex-column position-static">
+              <strong className="d-inline-block mb-2 text-success">
                 {capitalise(category.category_name)}
               </strong>
-              <h3 class="mb-0">{title}</h3>
-              <div class="mb-1 text-muted">
+              <h3 className="mb-0">{title}</h3>
+              <div className="mb-1 text-muted">
                 {month + " " + day}
               </div>
-              <p class="card-text mb-auto">
+              <p className="card-text mb-auto">
                 {excerpt}
               </p>
               <Link to={`/blog/${slug}`}>
                 Continue...
               </Link>
             </div>
-            <div class="col-auto d-none d-lg-block">
-              <img src={image} class="bd-placeholder-img" width="200" height="250" alt="post-img"/>
+            <div className="col-auto d-none d-lg-block">
+              <img src={image} className="bd-placeholder-img" width="200" height="250" alt="post-img"/>
               {/* <svg
-                class="bd-placeholder-img"
+                className="bd-placeholder-img"
                 width="200"
                 height="200"
                 xmlns="http://www.w3.org/2000/svg"
