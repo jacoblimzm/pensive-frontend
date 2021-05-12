2f4496f8de367bbcb291174322cf169c2d585c49
jacoblimzm <jacob_lim@live.com>
2021-05-12 11:58:24 +0800

    add new inputs

--- src/pages/NewBlogPage.jsx
+++ src/pages/NewBlogPage.jsx
@@ -1,62 +1,129 @@
 import { useForm, Form } from "../components/useForm";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const NewBlogPage = () => {
-    const [postSuccess, setPostSuccess] = useState({});
-    const initialValues = {
-        title: "",
-        excerpt: "",
-        category: "",
-        image: "",
-        month: "", // dropdown with three capital letters
-        day: "", // dropdown with digit days
-        content: "",
-        breaking: false, // boolean. drop down?
-    }
-    
-    const { formValues, handleInputChange } = useForm(initialValues);
-    
+  const [postSuccess, setPostSuccess] = useState({});
+  const initialValues = {
+    title: "",
+    excerpt: "",
+    category: "",
+    image: "",
+    month: "", // dropdown with three capital letters
+    day: "", // dropdown with digit days
+    content: "",
+    breaking: false, // boolean. drop down?
+  };
 
-    return ( 
-        <main className="container form-container">
+  const { formValues, handleInputChange } = useForm(initialValues);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(e.target.data);
+  };
+  return (
+    <main className="container form-container">
       <Form handleSubmit={handleSubmit}>
-        <h3 className="mb-3">REGISTER FOR AN ACCOUNT</h3>
+        <div className="col-9 col-sm-9 col-md-6 mb-3">
+          <img className="img-fluid" src="" />
+        </div>
+        <h3 className="mb-3">CREATE SOMETHING NEW</h3>
         <p>Please do not leave any fields blank.</p>
         <div className="col-9 col-sm-9 col-md-6 mb-3">
-          <label htmlFor="register-email" className="form-label">
-            Email
+          <label htmlFor="new-title" className="form-label">
+            Title (50 Char Max)
           </label>
           <input
             required
-            name="email"
+            name="title"
             value={formValues.email}
             onChange={handleInputChange}
-            type="email"
+            type="text"
             className="form-control"
-            id="register-email"
-            aria-describedby="emailField"
+            id="new-title"
+            aria-describedby="titleField"
+            maxLength="50"
           />
         </div>
         {/* small hack to break to a new line */}
-        <div class="w-100"></div> 
+        <div class="w-100"></div>
         <div className="col-9 col-sm-9 col-md-6 mb-3">
-          <label htmlFor="register-username" className="form-label">
-            Username
+          <label htmlFor="new-excerpt" className="form-label">
+            Excerpt (150 Char Max)
           </label>
           <input
             required
-            name="username"
+            name="excerpt"
             value={formValues.username}
             onChange={handleInputChange}
             type="text"
             className="form-control"
-            id="register-username"
-            aria-describedby="usernameField"
+            id="new-excerpt"
+            aria-describedby="excerptField"
+            maxLength="150"
           />
         </div>
         {/* small hack to break to a new line */}
-        <div class="w-100"></div> 
+        <div class="w-100"></div>
+        <div className="col-9 col-sm-9 col-md-6 mb-3">
+          <label htmlFor="" className="form-label"></label>
+          <input
+            required
+            name=""
+            value=""
+            onChange={handleInputChange}
+            type=""
+            className="form-control"
+            id=""
+            aria-describedby=""
+          />
+        </div>
+        <div class="w-100"></div>
+        <div className="col-9 col-sm-9 col-md-6 mb-3">
+          <label htmlFor="" className="form-label"></label>
+          <input
+            required
+            name=""
+            value=""
+            onChange={handleInputChange}
+            type=""
+            className="form-control"
+            id=""
+            aria-describedby=""
+          />
+        </div>
+        <div class="w-100"></div>
+        <div className="col-9 col-sm-9 col-md-6 mb-3">
+          <label htmlFor="" className="form-label"></label>
+          <input
+            required
+            name=""
+            value=""
+            onChange={handleInputChange}
+            type=""
+            className="form-control"
+            id=""
+            aria-describedby=""
+          />
+        </div>
+        <div class="w-100"></div>
+        <div className="col-9 col-sm-9 col-md-6 mb-3">
+          <label htmlFor="" className="form-label"></label>
+          <input
+            required
+            name=""
+            value=""
+            onChange={handleInputChange}
+            type=""
+            className="form-control"
+            id=""
+            aria-describedby=""
+          />
+        </div>
+
+        {/* small hack to break to a new line */}
+        <div class="w-100"></div>
         <div className="col-9 col-sm-9 col-md-6 mb-3">
           <label htmlFor="register-password" className="form-label">
             Password
@@ -75,33 +142,33 @@ const NewBlogPage = () => {
         {/* small hack to break to a new line */}
         <div class="w-100"></div>
         <div className="col-9 col-sm-9 col-md-6 mb-3">
-        <div className="form-check mb-3">
-          <input
-            name="staffstatus"
-            className="form-check-input"
-            type="checkbox"
-            value="staff-status"
-            id="staffCheckDefault"
-          />
-          <label className="form-check-label" htmlFor="staffCheckDefault">
-            Staff
-          </label>
-        </div>
+          <div className="form-check mb-3">
+            <input
+              name="staffstatus"
+              className="form-check-input"
+              type="checkbox"
+              value="staff-status"
+              id="staffCheckDefault"
+            />
+            <label className="form-check-label" htmlFor="staffCheckDefault">
+              Staff
+            </label>
+          </div>
         </div>
         <div class="w-100"></div>
         <div className="col-9 col-sm-9 col-md-6 mb-3">
-        {registerSuccess.success === false ? (
-          <p style={{ color: "red" }}>{registerSuccess.message}</p>
-        ) : null}
-        <button className="w-100 btn btn-lg btn-dark mb-2" type="submit">
-          Create
-        </button>
+          {postSuccess.success === false ? (
+            <p style={{ color: "red" }}>{postSuccess.message}</p>
+          ) : null}
+          <button className="w-100 btn btn-lg btn-dark mb-2" type="submit">
+            Create
+          </button>
         </div>
-        <Link to="/login">Have an account? Log In.</Link>
+        <Link to="/">Back to home</Link>
       </Form>
       <p className="mt-5 mb-2 text-muted">what are you thinking of?</p>
     </main>
-     );
-}
- 
+  );
+};
+
 export default NewBlogPage;
