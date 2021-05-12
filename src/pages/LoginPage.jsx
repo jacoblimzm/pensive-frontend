6ee31908ec4d2b3cde63a4588c585c48f99edefe
jacoblimzm <jacob_lim@live.com>
2021-05-12 17:38:51 +0800

    remove console logs

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -27,7 +27,7 @@ const LogInPage = () => {
         history.push("/");
       }
     } catch (err) {
-      console.log(err.response.data);
+      
       setLoginSuccess(err.response.data.success);
     }
   };
@@ -78,7 +78,7 @@ const LogInPage = () => {
             aria-describedby="passwordField"
           />
         </div>
-        <div class="w-100"></div> 
+        
         <div style={{textAlign: "center"}} className="col-9 col-sm-9 col-md-6 mb-3">
           {loginSuccess === false ? (
             <p style={{ color: "red" }}>Please Try Again</p>
